8a10ba124bf6234941f351ff4094a53469c0ba16
Alba Heidari <albaheidari@gmail.com>
2018-09-25 13:03:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var greeting ="Hello, everybody!";
-
-var specialGuest = "Neil deGrasse Tyson";
-
-var greetSpecialGuest = `Hello, ${specialGuest}!`;
-
+var greeting = "Hello everybody";
+ 
+var specialGuest = "Neil deGrasse Tyson"
+ 
+var greetSpecialGuest = "" + specialGuest + "!";
+ 
 var topic = "space";
-
-var conversation = `Let's talk about ${topic}.`;
-
-`
+ 
+var conversation = `${topic}`;
